8efe2e24e383011f557e8200a4ecab4e0263f23a
wrld2gr3g <gregoryanderson984@gmail.com>
2026-05-27 17:13:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,18 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
+  define: {
+    'process.env': {},
+    'global': 'window'
+  },
+  optimizeDeps: {
+    include: ['three', 'react-globe.gl']
+  },
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 2000,
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
   }
 })
