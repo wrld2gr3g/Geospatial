32815ecdd34577365a58a175039f5e03f24b7b0b
wrld2gr3g <gregoryanderson984@gmail.com>
2026-05-27 16:33:53 -0400

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import CulturalFlowDashboard from './cultural-flow-dashboard.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CulturalFlowDashboard />
+  </React.StrictMode>,
+)
