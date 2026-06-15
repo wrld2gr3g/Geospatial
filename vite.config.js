ce56f69220add09529181a8294656dcae04cda87
wrld2gr3g <gregoryanderson984@gmail.com>
2026-06-15 09:45:32 -0400

    Add server configuration with port 3000

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
@@ -16,5 +17,8 @@ export default defineConfig({
     commonjsOptions: {
       include: [/node_modules/]
     }
+  },
+  server: {
+    port: 3000
   }
 })
